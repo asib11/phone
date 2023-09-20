42b67fc3e71b98befdda4e640357864eab03625d
asib <ahmedasib1@gmail.com>
2023-09-20 21:26:50 +0600

    search bar work

--- js/phone.js
+++ js/phone.js
@@ -1,6 +1,6 @@
-const loadApiData = async ()=>{
+const loadApiData = async searchValue=>{
     try{
-        const url = `https://openapi.programming-hero.com/api/phones?search=iphone`;
+        const url = `https://openapi.programming-hero.com/api/phones?search=${searchValue}`;
         const res = await fetch(url);
         const data = await res.json();
         displayLoad(data.data);
@@ -14,21 +14,25 @@ const displayLoad = phoneData =>{
     console.log(phoneData);
     const phoneContainer = document.getElementById('phone-container');
     phoneContainer.innerText = '';
-    phoneData.forEach(phones=>{
+    phoneData.forEach(phone=>{
         const div = document.createElement('div');
         div.classList.add('col');
         div.innerHTML = `
             <div class="card h-100">
-                <img src="..." class="card-img-top" alt="...">
+                <img src="${phone.image}" class="card-img-top p-4" alt="...">
                 <div class="card-body">
-                    <h5 class="card-title">${phones.brand}</h5>
-                    <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                    <h5 class="card-title">${phone.brand}</h5>
+                    <p class="card-text">Model: ${phone.phone_name}</p>
                 </div>
             </div>
         `;
         phoneContainer.appendChild(div);
     });
-}
+};
+
+document.getElementById("search-btn").addEventListener('click', ()=>{
+    loadApiData(document.getElementById('search-text').value);
+})
 
 
-loadApiData()
+loadApiData('iphone')
